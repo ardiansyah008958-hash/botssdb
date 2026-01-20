07093e5f9928bafc7a4e537a53af4f2dd4356251
ardiansyah008958-hash <ardiansyah008958@gmail.com>
2026-01-20 16:04:55 +0700

    Update token list

--- token.js
+++ token.js
@@ -1,5 +1,6 @@
 {
   "tokens": [
-    "7942981764:AAEZ5jgtLPykVy_I652o8bb_ydLpQcwTxRk"
+    "7942981764:AAEZ5jgtLPykVy_I652o8bb_ydLpQcwTxRk",
+    "8367257033:AAGPJdMa0V0iPadVl37tja5gbwQxLzl4zM0"
   ]
 }
